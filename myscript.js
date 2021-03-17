e1cfaec9b916d4ce3a18fb0a21c5753300728dec
Sergiomarretta <sergio.marretta@libero.it>
2021-03-17 16:39:15 +0100

    Part 1

---
+++ myscript.js
@@ -0,0 +1,17 @@
+// CREAZIONE OGGETTO STUDENTE
+var student = {
+  age: 30,
+  name: "Mike",
+  faculty: "it",
+  credits: 22,
+  sports: "football",
+  hobbies: ["music","gaming","hunting"]
+
+}
+
+// STAMPA OGGETTO
+
+for (var key in student) {
+console.log(key);
+}
+// INTERAZIONE UTENTE AGGIUNTA OGGETTI
